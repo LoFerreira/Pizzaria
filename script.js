d0fb9c22f41d17fe92472577a8b37b0d39bc944c
Leonardo Ferreira <leonardo10sp@gmail.com>
2021-06-10 01:15:44 -0300

    mostrando o nome das pizzas

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ pizzaJson.map((item, index) => {
     //PREENCHER AS INFORMAÇÕES EM pizzaItem
     let pizzaItem = c('.models .pizza-item').cloneNode(true);
 
-    pizzaItem.
+    pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name
 
     //MOSTRANDO OS MODELOS DE PIZZA NA TELA
     c('.pizza-area').append(pizzaItem);
