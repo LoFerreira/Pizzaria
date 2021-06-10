09c136c0a9471b0c5f0b8745eab7ff6be0be3c63
Leonardo Ferreira <leonardo10sp@gmail.com>
2021-06-10 01:21:14 -0300

    foto das pizzas

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ pizzaJson.map((item, index) => {
     pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name
     pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description
     pizzaItem.querySelector('.pizza-item--price').innerHTML = `RS ${item.price.toFixed(2)}`
+    pizzaItem.querySelector('.pizza-item--img img').src = item.img
 
     //MOSTRANDO OS MODELOS DE PIZZA NA TELA
     c('.pizza-area').append(pizzaItem);
