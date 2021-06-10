5c854a0ce8789f092bb03b85145804c9ed48f379
Leonardo Ferreira <leonardo10sp@gmail.com>
2021-06-10 16:29:28 -0300

    configurando a pizza clicada no modal page

--- script.js
+++ script.js
@@ -5,20 +5,32 @@ pizzaJson.map((item, index) => {
     //PREENCHER AS INFORMAÇÕES EM pizzaItem ===============================
     let pizzaItem = c('.models .pizza-item').cloneNode(true);
 
-    //NOME, DESCRIÇÃO, PREÇO E IMAGEM DAS PIZZAS ===============================
+    //NOME, DESCRIÇÃO, PREÇO, IMAGEM DAS PIZZAS E O INDEX ===============================
     pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name
     pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description
     pizzaItem.querySelector('.pizza-item--price').innerHTML = `RS ${item.price.toFixed(2)}`
     pizzaItem.querySelector('.pizza-item--img img').src = item.img
-
+    pizzaItem.setAttribute('data-key', index);
+    
+    //MOSTRANDO A MODAL PAGE NA TELA  ===============================
     pizzaItem.querySelector('a').addEventListener('click', (e) => {
         e.preventDefault();
 
+        //ABRINDO O MODAL PAGE  ===============================
         c('.pizzaWindowArea').style.opacity = 0;
         c('.pizzaWindowArea').style.display = 'flex';
         setTimeout(() => {
             c('.pizzaWindowArea').style.opacity = 1;
         }, 200);
+
+        //PEGANDO O ID DA PIZZA CLICADA  ===============================
+        let key = e.target.closest('.pizza-item').getAttribute('data-key');
+
+        //PUXANDO OS DADOS DA PIZZA CLICADA E MOSTRANDO NO MODAL PAGE  ===============================
+        c('.pizzaInfo h1').innerHTML = pizzaJson[key].name; 
+        c('.pizzaInfo--desc').innerHTML = pizzaJson[key].description; 
+        c('.pizzaBig img').src = pizzaJson[key].img;
+        
 
     })
 
