884944720b71d7b7035a5cb309d9f4c391b69333
Leonardo Ferreira <leonardo10sp@gmail.com>
2021-06-10 16:06:50 -0300

    abertura da modal page

--- script.js
+++ script.js
@@ -2,14 +2,21 @@ const c = (e) => document.querySelector(e);
 const ca = (e) => document.querySelectorAll(ca);
 
 pizzaJson.map((item, index) => {
-    //PREENCHER AS INFORMAÇÕES EM pizzaItem
+    //PREENCHER AS INFORMAÇÕES EM pizzaItem ===============================
     let pizzaItem = c('.models .pizza-item').cloneNode(true);
 
+    //NOME, DESCRIÇÃO, PREÇO E IMAGEM DAS PIZZAS ===============================
     pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name
     pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description
     pizzaItem.querySelector('.pizza-item--price').innerHTML = `RS ${item.price.toFixed(2)}`
     pizzaItem.querySelector('.pizza-item--img img').src = item.img
 
-    //MOSTRANDO OS MODELOS DE PIZZA NA TELA
+    pizzaItem.querySelector('a').addEventListener('click', (e) => {
+        e.preventDefault();
+
+        c('.pizzaWindowArea').style.display = 'flex';
+    })
+
+    //MOSTRANDO OS MODELOS DE PIZZA NA TELA ===============================
     c('.pizza-area').append(pizzaItem);
 });
