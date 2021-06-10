708eda2a75ee8da53b2b5b1be48a369df85856eb
Leonardo Ferreira <leonardo10sp@gmail.com>
2021-06-10 18:06:56 -0300

    evento de fechar modal

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
+let modalQT = 1;
+
 const c = (e) => document.querySelector(e);
 const ca = (ca) => document.querySelectorAll(ca);
 
+//lISTAGEM DAS PIZZAS ===============================
 pizzaJson.map((item, index) => {
+
     //PREENCHER AS INFORMAÇÕES EM pizzaItem ===============================
     let pizzaItem = c('.models .pizza-item').cloneNode(true);
 
@@ -26,17 +30,37 @@ pizzaJson.map((item, index) => {
         //PEGANDO O ID DA PIZZA CLICADA  ===============================
         let key = e.target.closest('.pizza-item').getAttribute('data-key');
 
+        //RESETANDO A QUANTIDADE DE PIZZAS DA MODAL PAGE  ===============================
+        modalQT = 1;
+
         //PUXANDO OS DADOS DA PIZZA CLICADA E MOSTRANDO NO MODAL PAGE  ===============================
         c('.pizzaInfo h1').innerHTML = pizzaJson[key].name; 
         c('.pizzaInfo--desc').innerHTML = pizzaJson[key].description; 
         c('.pizzaBig img').src = pizzaJson[key].img;
-        c('.pizzaInfo--actualPrice').innerHTML = `RS ${pizzaJson[key].price.toFixed(2)}`
+        c('.pizzaInfo--actualPrice').innerHTML = `RS ${pizzaJson[key].price.toFixed(2)}`;
+        c('.pizzaInfo--size.selected').classList.remove('selected');
         ca('.pizzaInfo--size').forEach((size, sizeIndex) => {
+            if(sizeIndex == 2) {
+                size.classList.add('selected');
+            }
             size.querySelector('span').innerHTML = pizzaJson[key].sizes[sizeIndex];
-        })
+        });
 
-    })
+        c('.pizzaInfo--qt').innerHTML = modalQT;
+    });
 
     //MOSTRANDO OS MODELOS DE PIZZA NA TELA ===============================
     c('.pizza-area').append(pizzaItem);
 });
+
+//EVENTOS DO MODAL ===============================
+
+
+//FECHANDO O MODAL ===============================
+function closeModal() {
+    c('.pizzaWindowArea').style.opacity = 0;
+    setTimeout(() => {
+        c('.pizzaWindowArea').style.display = 'none';
+    }, 500);
+}
+
